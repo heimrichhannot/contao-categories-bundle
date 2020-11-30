99ab2070fc192ff2ece1884372df94f81c70ac20
Thomas Körner <t.koerner@heimrich-hannot.de>
2020-11-30 10:43:05 +0100

    added new model methods and model annotations

--- src/Model/CategoryModel.php
+++ src/Model/CategoryModel.php
@@ -8,7 +8,38 @@
 
 namespace HeimrichHannot\CategoriesBundle\Model;
 
-class CategoryModel extends \Contao\Model
+use Contao\Model;
+use Contao\Model\Collection;
+
+/**
+ * Class CategoryModel.
+ *
+ * @property int         $id;
+ * @property int         $pid;
+ * @property int         $sorting;
+ * @property int         $tstamp;
+ * @property int         $dateAdded;
+ * @property string      $title;
+ * @property string      $frontendTitle;
+ * @property string      $alias;
+ * @property string      $cssClass;
+ * @property string      $jumpTo;
+ * @property string|bool $selectable;
+ */
+class CategoryModel extends Model
 {
     protected static $strTable = 'tl_category';
+
+    /**
+     * @return CategoryModel|CategoryModel[]|Collection|null
+     */
+    public function getChildCategories()
+    {
+        return static::findBy([static::$strTable.'.pid=?'], [$this->id]);
+    }
+
+    public function hasChildCategories(): bool
+    {
+        return null !== $this->getChildCategories();
+    }
 }
